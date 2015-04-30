81448ca49a84ac27b5d76aa58e741351f437b564
Willam <wimo7083@colorado.edu>
2015-04-29 21:46:27 -0600

    removed command line

--- main.cpp
+++ main.cpp
@@ -8,10 +8,12 @@
 
 #include <iostream>
 #include "Graph.h"
-int main(int argc, const char * argv[])
+//int main(int argc, const char * argv[])
+int main()
 {
     graph RG;//RG for Reaction Graph
-    RG.build_graph(argv[1]);
+    RG.build_graph("reactionlist.txt");
+    //RG.build_graph(argv[1]);
     bool repeat(1);
     vector<ver *> names;
     RG.namesList(names);
@@ -20,7 +22,7 @@ int main(int argc, const char * argv[])
         cout << i+1 << ". ";
         cout << names[i]->name << endl;
     }
-    
+
     while(repeat)
     {
         cout << "From the printed list, Enter the chemicals' codes you wish to use. Enter 0 when you are finished." << endl;
@@ -32,14 +34,14 @@ int main(int argc, const char * argv[])
             cout << "You should choose at least one chemical as reactors. Enter the chemicals' code you wish to use."<< endl;
             cin >> i;
         }
-        
+
         while( i != 0 )
         {
             reactors.push_back(names[i-1]);
             cin >> i;
         }
         RG.makeReactors_av(reactors);
-        
+
         cout << "Now enter the products' codes you wish to obtain:" << endl;
         vector<ver *> products;
         size_t j;
@@ -49,7 +51,7 @@ int main(int argc, const char * argv[])
             cout << "You should choose at least one chemical as products. Enter the chemicals' code you wish to obtain."<< endl;
             cin >> j;
         }
-        
+
         while( j != 0 )
         {
             products.push_back(names[j-1]);
@@ -66,6 +68,6 @@ int main(int argc, const char * argv[])
         if( repeat )
             RG.make_default();
     }
-    
+
     return 0;
 }
